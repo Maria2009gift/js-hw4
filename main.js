46f9f730619d3c7284dc0a374dcad078dace95bd
Maria2009gift <kovalenkom2009@gmail.com>
2023-06-27 19:40:41 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+
+// -----1-----
+
+const bigWord = document.querySelector(".word")
+bigWord.textContent = "Змінене слово"
+
+const button = document.querySelector(".button")
+button.textContent = "Змінена кнопка"
+
+// -----2-----
+
+const cats = document.querySelector(".cats")
+cats.src="cat2.jpg"
+
+// -----3-----
+
+const aboutCats = document.querySelector(".wiki")
+aboutCats.href = "https://uk.wikipedia.org/wiki/%D0%9A%D1%96%D1%82_%D1%81%D0%B2%D1%96%D0%B9%D1%81%D1%8C%D0%BA%D0%B8%D0%B9"
+
+const imgWithCats = document.querySelector(".many-cats")
+imgWithCats.alt = "двоє котів"
+
+// -----4-----
+
+const listWithItem = document.querySelector(".item")
+listWithItem.textContent = "item change 1"
